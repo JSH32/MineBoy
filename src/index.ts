3ad9b444c3740f067b0bb7f694c228763a387153
JSH32 <rudnik7000@gmail.com>
2022-08-22 20:06:28 -0500

    Use fastify instead of express

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import "dotenv/config";
 import Gameboy from "serverboy";
-import express from "express";
-import expressWs from "express-ws";
 import { match, P } from "ts-pattern";
 import { useTry } from "no-try";
 import ws from "ws";
@@ -14,11 +12,14 @@ import { FixedArray } from "./utils";
 import zlib from "zlib";
 import { promisify } from "util";
 
+import Fastify from 'fastify';
+import FastifyWebsocket from '@fastify/websocket';
+
+const fastify = Fastify();
+fastify.register(FastifyWebsocket)
+
 // Pretty colors
 new Logger({ name: "console", overwriteConsole: true });
-
-const wsInstance = expressWs(express());
-const app = wsInstance.app;
 
 // Gameboy resolution
 const WIDTH = 160;
@@ -104,98 +105,110 @@ const quantizeFrame = (
 const games = getGames(path.join(process.cwd(), "roms"));
 console.info(`Loaded ${Object.keys(games).length} roms:`, Object.keys(games));
 
-app.get("/listGames", (_, res) =>
-    res.send(serialize(Object.keys(games))));
+// app.get("/listGames", (_, res) =>
+//     res.send(serialize(Object.keys(games))));
+
+fastify.get('/listGames', async () =>
+    serialize(Object.keys(games)))
 
 // Any API that does operations on the gameboy should be completely asynchronous and require a WS session
-app.ws("/attach", (ws: ws) => {
-    console.info(`Client connected (${wsInstance.getWss().clients.size} online)`);
+fastify.register(async (fastify) => {
+    fastify.get('/*', { websocket: true }, (conn) => {
+        console.info(`Client connected (${fastify.websocketServer.clients.size} online)`);
 
-    const gameboy = new Gameboy();
-    const keysPressed = new Map<number, number>();
-    let gameName = null;
+        const gameboy = new Gameboy();
+        const keysPressed = new Map<number, number>();
+        let gameName = null;
 
-    // 120fps without spamming the eventloop
-    // This is null util a game is running
-    let intervalId = null;
+        // 120fps without spamming the eventloop
+        // This is null util a game is running
+        let intervalId = null;
 
-    ws.on("message", async (msgString: string) => {
-        const [error, res] = useTry<any>(() => deserialize(Buffer.from(msgString)));
-        if (error) return sendError(ws, "Invalid data provided");
+        conn.socket.on('message', (message: ws.RawData) => {
+            const [error, res] = useTry<any>(() => deserialize(message as any));
+            if (error) return sendError(conn.socket, "Invalid data provided");
 
-        match(res)
-            .with({ type: "SELECT_GAME", index: P.number, save: P.optional(P.any) }, async () => {
-                if (res.index > games.length)
-                    return sendError(ws, "Invalid game index selected");
+            match(res)
+                .with({ type: "SELECT_GAME", index: P.number, save: P.optional(P.any) }, async () => {
+                    if (res.index > games.length)
+                        return sendError(conn.socket, "Invalid game index selected");
 
-                const saveData = res.save ? await promisify(zlib.inflate)(Buffer.from(res.save, "base64")) : undefined;
-                gameName = Object.keys(games)[res.index];
-                gameboy.loadRom(games[gameName], saveData);
+                    const saveData = res.save ? await promisify(zlib.inflate)(Buffer.from(res.save, "base64")) : undefined;
+                    gameName = Object.keys(games)[res.index];
+                    gameboy.loadRom(games[gameName], saveData);
 
-                if (!intervalId) {
-                    intervalId = setInterval(() => {
-                        const keysToPress = [];
+                    if (!intervalId) {
+                        intervalId = setInterval(() => {
+                            const keysToPress = [];
 
-                        // For some reason not every keystroke will register the first time so we make it click on three updates
-                        for (const [key, count] of keysPressed) {
-                            if (count > 0) {
-                                keysToPress.push(key);
-                                keysPressed.set(key, count - 1);
+                            // For some reason not every keystroke will register the first time so we make it click on three updates
+                            for (const [key, count] of keysPressed) {
+                                if (count > 0) {
+                                    keysToPress.push(key);
+                                    keysPressed.set(key, count - 1);
+                                }
                             }
-                        }
 
-                        gameboy.pressKeys(keysToPress);
-                        gameboy.doFrame();
-                    }, 1000 / 120);
-                }
+                            gameboy.pressKeys(keysToPress);
+                            gameboy.doFrame();
+                        }, 1000 / 120);
+                    }
 
-                ws.send(serialize({ type: "GAME_STARTED", name: gameName }));
-            })
-        // Handler for other routes which require game to be running
-            .otherwise(() => {
-                if (!intervalId)
-                    return sendError(ws, "No game running");
+                    conn.socket.send(serialize({ type: "GAME_STARTED", name: gameName }));
+                })
+                // Handler for other routes which require game to be running
+                .otherwise(() => {
+                    if (!intervalId)
+                        return sendError(conn.socket, "No game running");
 
-                match(res)
-                    .with({ type: "EXIT_GAME" }, () => {
-                        clearInterval(intervalId);
-                        intervalId = null;
+                    match(res)
+                        .with({ type: "EXIT_GAME" }, () => {
+                            clearInterval(intervalId);
+                            intervalId = null;
 
-                        ws.send(serialize({ type: "GAME_EXITED" }));
-                    })
-                    .with({ type: "GET_SAVE" }, () => {
+                            conn.socket.send(serialize({ type: "GAME_EXITED" }));
+                        })
+                        .with({ type: "GET_SAVE" }, () => {
                         // Compress save due to limited disk storage
-                        zlib.deflate(Buffer.from(gameboy.getSaveData()), (_, buffer) => {
-                            ws.send(serialize({
-                                type: "SAVE_DATA",
-                                gameName,
-                                data: buffer.toString("base64")
+                            zlib.deflate(Buffer.from(gameboy.getSaveData()), (_, buffer) => {
+                                conn.socket.send(serialize({
+                                    type: "SAVE_DATA",
+                                    gameName,
+                                    data: buffer.toString("base64")
+                                }));
+                            });
+                        })
+                        .with({ type: "PRESS_BUTTON", button: P.string }, () => {
+                            keysPressed.set(Gameboy.KEYMAP[res.button], 10);
+                        })
+                        .with({ type: "REQUEST_DRAW" }, async () => {
+                            const [palette, output] = quantizeFrame(gameboy.getScreen(), [WIDTH, HEIGHT]);
+
+                            conn.socket.send(serialize({
+                                type: "SCREEN_DRAW",
+                                width: WIDTH,
+                                height: HEIGHT + 1, // Add one line to compensate for invisible line
+                                screen: output,
+                                palette: palette
                             }));
-                        });
-                    })
-                    .with({ type: "PRESS_BUTTON", button: P.string }, () => {
-                        keysPressed.set(Gameboy.KEYMAP[res.button], 10);
-                    })
-                    .with({ type: "REQUEST_DRAW" }, async () => {
-                        const [palette, output] = quantizeFrame(gameboy.getScreen(), [WIDTH, HEIGHT]);
+                        })
+                        .otherwise(() => console.warn("Caught invalid request:", res));
+                });
+        })
 
-                        ws.send(serialize({
-                            type: "SCREEN_DRAW",
-                            width: WIDTH,
-                            height: HEIGHT + 1, // Add one line to compensate for invisible line
-                            screen: output,
-                            palette: palette
-                        }));
-                    })
-                    .otherwise(() => console.warn("Caught invalid request:", res));
-            });
-    });
-
-    ws.on("close", () => {
-        console.info(`Client disconnected (${wsInstance.getWss().clients.size} online)`);
-        if (intervalId)
-            clearInterval(intervalId);
-    });
+        conn.socket.on("close", () => {
+            console.info(`Client disconnected (${fastify.websocketServer.clients.size} online)`);
+            if (intervalId)
+                clearInterval(intervalId);
+        });
+    })
 });
 
-app.listen(process.env.PORT, () => console.info(`Listening on http://localhost:${process.env.PORT}`));
+fastify.listen({ port: parseInt(process.env.PORT, 10) }, err => {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+
+    console.info(`Listening on http://localhost:${process.env.PORT}`);
+})
